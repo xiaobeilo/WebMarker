251f8f9361b0411c505ad9e8e3adb9a39af1334b
cMing <mayday404@163.com>
2016-11-08 19:02:30 +0800

    书签同目录下的dl没有查找出来

--- js/index.js
+++ js/index.js
@@ -8,9 +8,10 @@ var startX = 0,startY=0;
 var timer = null;
 var mask = document.getElementById('mask');
 window.document.addEventListener('touchstart',function(e){
+    var target = e.target || e.srcElement;
     startX = e.targetTouches[0].pageX;
     startY = e.targetTouches[0].pageY;
-    timer = setTimeout(function(){showSearch();},800)
+    if(target.id==='main'){timer = setTimeout(function(){showSearch();},800)}
 });
 var originX = getOffset(nav);
 window.document.addEventListener('touchmove',function(e){
@@ -82,8 +83,6 @@ var ipt = new Vue({
     }
 
 });
-
-
 function getOffset(ele){
     var reg = /matrix\(\s?1,\s?0,\s?0,\s?1,([^,]+),\s?0\)/;
     return parseFloat(getComputedStyle(nav).transform.match(reg)[1]);
@@ -95,22 +94,78 @@ function transform(ele,prop){
     }
 }
 /*滚动条*/
-var scroll = document.getElementsByClassName('scroll');
-for(var i=0;i<scroll.length;i++){
-    !function(){
-        var offset = 0;
-        var parentEle = scroll[i].parentNode;
-        var container = parentEle.getElementsByClassName('container')[0]
-        parentEle.addEventListener('mousewheel',function(e){
-            console.log(getComputedStyle(this).height);
-            /*计算滚动范围*/
-            offset += e.deltaY;
-            container.style.transform = 'translateY('+-offset+'px)';
-            Velocity(container,'finish');
-            Velocity(container,{
-                translateY:-offset+'px'
-            },300);
-
-        })
-    }();
+Scrollbar.init(document.getElementsByClassName('scl_container')[0],{
+    overscrollEffect:'bounce'
+});
+//书签整理:
+var bookmarks = {};
+var bookmarksOrigin = '';
+window.document.addEventListener('dragover',function(e){
+    e.preventDefault();
+    // e.dataTransfer.dropEffect = 'copy';
+},false);
+window.document.addEventListener('drop',function(e){
+    e.preventDefault();
+    var reader = new FileReader();
+    reader.readAsText(e.dataTransfer.files[0]);
+    reader.onload = function(){
+        bookmarksOrigin = this.result;
+        dealStr(bookmarksOrigin);
+        /*这里最后使用webworker*/
+    }
+},false);
+function dealStr(str){
+    var dlChace = '';
+    var f1 = /<H1>([\s\S]+)<\/H1>\s*(<DL>[\s|\S]+<\/DL>)/i;
+    var f1Result  = str.match(f1);
+    // console.log(f1Result[2]);
+    bookmarks.h1 = f1Result[1];
+    findDl(bookmarks,f1Result[2]);
+    console.log(bookmarks);
+    function findDl(obj,str){
+        str = trim(str);
+        // document.getElementById('main').innerText = str;
+        var reg = /<H3[^>]+>([^<]*)<\/H3>\s*(<DL>[\s|\S]+<\/DL>)/i;
+        var result = str.match(reg);
+        siblingsDl(result[2]);
+        if(result){
+            obj.dl ={
+                h3:result[1],
+                dt:getDt(str)
+            };
+            findDl(obj.dl,result[2]);
+        }else{
+            obj.dt = getDt(str);
+        }
+    }
+    function getDlList(str){
+        str = removeDl(str);
+    }
+    function hasDl(str){
+        return /<DL>/.test(str);
+    }
+    function trim(str){
+        return str.replace(/^\s*<DL>\s*<p>|<\/DL>$/ig,'');//去掉前尾dl标签
+    }
+    function removeDl(str){
+        return str.replace(/<DL>[\s|\S]+<\/DL>/ig,'');//去除dl只剩下dt
+    }
+    function getDt(str){
+        str = removeDl(str);
+        var dtList = [];
+        var dtListStr = str.match(/<DT><A\s*HREF="([^"]+)"[^>]+>([^<]*)<\/A>/ig);
+        for(var i=0;i<dtListStr.length;i++){
+            var result = dtListStr[i].match(/<DT><A\s*HREF="([^"]+)"[^>]+>([^<]*)<\/A>/i);
+            dtList[dtList.length] = {
+                href:result[1],
+                name:result[2]
+            };
+        }
+        return dtList;
+    }
+    function siblingsDl(str){
+        str = trim(str);
+        var reg = /<DL>[\s|\S]+?(\s*)<\/DL>/ig;
+        var result = str.match(reg);
+    }
 }
