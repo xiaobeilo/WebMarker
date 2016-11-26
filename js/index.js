e168c2713924dd5208fa90d97a73541657c7b414
cMing <mayday404@163.com>
2016-11-26 20:39:38 +0800

    移动端呼出搜索框和解析书签存在bug

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ var nav = document.getElementById('nav');
 var search_box = document.getElementById('search_box');
 var navWidth = parseFloat(getComputedStyle(nav).width);
 var startX = 0,startY=0;
+var endX=0,endY=0;
 var timer = null;
 var mask = document.getElementById('mask');
 var  link_container = document.getElementsByClassName('link_container')[0];
@@ -17,32 +18,46 @@ window.document.addEventListener('touchstart',function(e){
     while(target.nodeName!=='SECTION' && target.parentNode){
         target = target.parentNode;
     }
-    if(target.id==='main'){timer = setTimeout(function(){showSearch();},800)}
+    if(target.id==='main'){timer = setTimeout(function(){},800)}
 });
 var originX = getOffset(nav);
 window.document.addEventListener('touchmove',function(e){
-    clearTimeout(timer);
-   if(getComputedStyle(mask).zIndex<0){
-       var sx = startX;
-       var ex = e.targetTouches[0].pageX;
-       nav.style.transform = 'matrix(1,0,0,1,'+ (originX-(sx-ex)) +',0)';
-       nav.style.zIndex = '3';
-   }
+    var sx = startX;
+    var sy = startY;
+    endX = e.targetTouches[0].pageX;
+    if(getComputedStyle(mask).zIndex<0 && Math.abs(sx-endX)>50){
+       transform(nav,'matrix(1,0,0,1,'+ (originX-(sx-endX)) +',0)')
+    }
+    if(Math.abs(endX-sx)===0 && Math.abs(endY-sy)===0){
+        endX=0;endY=0;
+    }
 },false);
 window.document.addEventListener('touchend',function(e){
-    timer && clearTimeout(timer);
-    nav.className = 'sliderEnd';
-    if(getOffset(nav) > -100){
-        nav.style.transform = 'matrix(1,0,0,1,0,0)';
+    console.log(endY,endX)
+    if(endX===0 && endY===0){
+        showSearch();
     }else{
-        nav.style.transform = 'matrix(1,0,0,1,'+-Math.ceil(navWidth)+',0)';
-        nav.style.zIndex = '1';
+        clearTimeout(timer);
     }
-    nav.addEventListener('transitionend',function(){
-        nav.className='';
-        originX = getOffset(nav);
-    });
+    nav.className = 'sliderEnd';
+    var target = e.target;
+    while(target.id!=='nav' && target.parentNode){
+        target = target.parentNode;
+    }
+    getOffset(nav) > -100? transform(nav,'matrix(1,0,0,1,0,0)'):hideNav();
+    (target.id!=='nav' && getOffset(nav)===0)&&hideNav();
+    startX=0;startY=0;endX=0;endY=0;
 },false);
+function hideNav(){
+    transform(nav,'matrix(1,0,0,1,'+-Math.ceil(navWidth)+',0)')
+    nav.addEventListener('transitionend',function(){
+        this.removeEventListener('transitionend',arguments.callee);
+    });
+}
+nav.addEventListener('transitionend',function(){
+    nav.className='';
+    originX = getOffset(nav);
+});
 window.document.addEventListener('keydown',function(e){
     if(e.code ==='Space'){
         showSearch();
@@ -58,27 +73,33 @@ mask.addEventListener('click',function(e){
     var target  = e.target || e.srcElement;
     if(target.id==='mask'){hideSearch()}
 },false);
+var handbook = document.getElementById('handbook');
+document.getElementById('handbook_btn').addEventListener('click',function(e){
+    e.preventDefault();
+    handbook.style.display = 'block';
+},false);
+handbook.addEventListener('click',function(e){
+    if(e.target.id==='handbook'){
+        this.style.display = 'none';
+    }
+},false)
 function showSearch(){
     var input = search_box.getElementsByTagName('input')[0];
     mask.style.zIndex = 1000;
-    Velocity(mask,'finish');
-    Velocity(mask,{
-        backgroundColor:'#000',
-        backgroundColorAlpha:0.3
-    },300);
-    search_box.style.transform = 'translateY(0)';
+    mask.style.backgroundColor = 'rgba(0,0,0,0.3)';
+    transform(search_box,'translateY(0)');
     setTimeout(function(){input.focus()},200)
 }
 function hideSearch(){
     var input = search_box.getElementsByTagName('input')[0];
-    search_box.style.transform = 'translateY(-100%)';
-    Velocity(mask,'finish');
-    Velocity(mask,'reverse',function(){
-        mask.style.zIndex = -1;
-        input.value = '';input.blur();
-        ipt.clear();
-    });
+    transform(search_box,'translateY(-100%)');
+    mask.style.backgroundColor = 'rgba(0,0,0,0.01)';
+    mask.addEventListener('transitionend',function(){
+        this.style.zIndex = -1;
+        this.removeEventListener('transitionend',arguments.callee);
+    },false);
 }
+
 
 //vue:
 var ipt = new Vue({
@@ -120,12 +141,6 @@ function getOffset(ele){
     var reg = /matrix\(\s?1,\s?0,\s?0,\s?1,([^,]+),\s?0\)/;
     return parseFloat(getComputedStyle(nav).transform.match(reg)[1]);
 }
-function transform(ele,prop){
-    var arr = ['transform','WebKitTransform','MozTransform','OTransform','MsTransform'];
-    for(var i=0;i<4;i++){
-        ele.style[arr[i]] = prop;
-    }
-}
 /*滚动条*/
 Scrollbar.init(document.getElementsByClassName('scl_container')[0],{
     overscrollEffect:'bounce'
@@ -135,6 +150,9 @@ Scrollbar.init(document.getElementsByClassName('result_container')[0],{
 });
 Scrollbar.init(document.getElementsByClassName('link_container')[0],{
     overscrollEffect:'glow'
+});
+Scrollbar.init(document.getElementsByClassName('bandbook_container')[0],{
+    overscrollEffect:'bounce'
 });
 document.getElementsByClassName('link_container')[0].style.overflowX='visible';
 document.getElementsByClassName('link_container')[0].style.overflowY='hidden';
@@ -155,13 +173,14 @@ window.document.addEventListener('drop',function(e){
         dealStr(bookmarksOrigin);
         watchMark.bookmarks = bookmarks;
         marks.list = bookmarks[0].b;
-        document.getElementsByClassName('scroll-content')[0].style.transform = 'translate3d(0,0,0);'
+        // document.getElementsByClassName('scroll-content')[0].style.transform = 'translate3d(0,0,0);'
+        transform(document.getElementsByClassName('scroll-content')[0],'translate3d(0,0,0)')
         save.show = true;
         ipt.bmList = getBmList(bookmarks);
-        /*这里最后使用webworker*/
+        /*这里可以考虑使用webworker*/
     }
 },false);
-function dealStr(str){
+function dealStr(str){//所有处理书签的逻辑
     str  = dealSim(str);
     if(checkBrowser(str)==='mozilla'){
         str = cutMozilla(str);
@@ -223,7 +242,7 @@ function dealStr(str){
                 var result = dtListStr[i].match(/<A\sHREF="([^"]+)">([^<]*)<\/A>/);
                 var name = result[2],tit='',desc='';
                 if(name){
-                    var nameResult = name.match(/^([^,，\s\-\_|]+)[,，\s\-\|]*(.*)/);
+                    var nameResult = name.match(/^([^,，\-\_|]+)[,，\s\-\|]*(.*)/);
                     if(nameResult){
                         tit = nameResult[1];
                         desc = nameResult[2];
@@ -311,7 +330,7 @@ var marks = new Vue({
     data:{
         list:[]
     }
-})
+});
 var watchMark = new Vue({
     el: '#nav_folder',
     data:{
@@ -338,8 +357,8 @@ var save = new Vue({
                     if(text !== '-1'){
                         this.show = false;
                         alert('书签保存成功!点击确定为您跳转到新的书签导航页:\nhttp://webmarker.youledi.cn/#'+text)
-                        // window.location.href = 'http://webmarker.youledi.cn/#'+text;
-                        // window.location.reload();
+                        window.location.href = 'http://webmarker.youledi.cn/#'+text;
+                        window.location.reload();
                     }else{
                         alert('书签保存失败..请充实==重试');
                     }
@@ -456,3 +475,11 @@ var targetChange = new Vue({
         }
     }
 });
+
+//兼容性封装
+function transform(ele,prop){
+    var arr = ['transform','WebKitTransform','MozTransform','OTransform','MsTransform'];
+    for(var i=0;i<arr.length;i++){
+        ele.style[arr[i]] = prop;
+    }
+}
